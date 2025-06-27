20e25ae833583d0aa24d07931c97f02d720facfa
gvg03 <127883426+gvg03@users.noreply.github.com>
2025-06-26 19:39:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 
 #home age setup
 
-
 # Imports
-
 import streamlit as st # importing streamlit
 import pandas as pd    # panda
 import plotly
@@ -12,25 +10,46 @@ import json            # to load file, for zipcode
 import streamlit.components.v1 as components   # to bring in PowerBi
 
 # Page Setup, layout and title
-
 st.set_page_config(layout="wide")
 st.title("YWCA & UTSA SDS Innovation Scholars")
- 
-program_type = st.sidebar.selectbox("Select a Program:", ["HRSA", "RESET", "RTW"])
 
-# placeholder to test
-if program_type == "HRSA":
-    st.metric("Total Participants", 111)
-    st.metric("Employed Participants", 30)
+# Sidebar: Navigate + Prog Filter
+st.sidebar.title("Program Explorer")
+page = st.sidebar.radio("View", ["Home", "Dashboard", "Map"])
 
-elif program_type == "RESET":
-    st.metric("Total Participants", 85)
+program = st.sidebar.selectbox("Select Program:", ["HRSA", "RESET", "RTW"])
+
+# HOME page landing
+if page == "Home":
+    st.title("YWCA & UTSA SDS Community Innovation Scholars")
+ # HRSA PAge
+    if program == "HRSA":
+        st.metric("Total Participants", 100)
+        st.metric("Employed Participants", 20)
+     
+    st.subheader("About the HRSA Program")
+    st.write(
+        "BIO BLANK."
+    )
+
+ # RESET Page
+if program == "RESET":
+    st.metric("Total Participants", 200)
     st.metric("Employed Participants", 40)
-
-elif program_type == "RTW":
-    st.metric("Total Participants", 401)
-    st.metric("Employed Participants", 100)
+     
+    st.subheader("About the HRSA Program")
+    st.write(
+        "BIO BLANK."
+    )
  
-
+ # RTW Page
+if program == "RTW":
+        st.metric("Total Participants", 401)
+        st.metric("Employed Participants", 50)
+     
+    st.subheader("About the HRSA Program")
+    st.write(
+        "BIO BLANK."
+    )
 
 
