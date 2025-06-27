4e192ff5c9f5814fd30ee5c8bb960b62c63fbb25
gvg03 <127883426+gvg03@users.noreply.github.com>
2025-06-26 21:09:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,16 +18,15 @@ program = st.sidebar.selectbox("Select Program:", ["-- Select a Program --", "HR
 
 # Landing Page
 if program == "-- Select a Program --":
-    # ✅ Fix: incorrect syntax here — should be = not -
+    # creating columns for pics
     col1, col2 = st.columns([1, 1])
 
     with col1:
         st.image(
             "https://images.squarespace-cdn.com/content/v1/654d110ae6bee3661f065bbd/a0f0fdba-f230-436e-9386-8ad375390c74/logo-orange_1_.png",
-            width=150
+            width=150 
         )
     
-    # ✅ Fix: this should be col2, not col1 again
     with col2:
         st.image(
             "https://sds.utsa.edu//community_scholars/images/cis-for-color-background.png",
@@ -51,3 +50,38 @@ else:
         st.title(f"{program} - Program Overview")
 
         if program == "HRSA":
+            st.metric("Total Participants", 100)
+            st.metric("Employed Participants", 20)
+            st.subheader("About")
+            st.write("meh meh")
+
+        elif program == "RESET":
+            st.metric("Total Participants", 200)
+            st.metric("Employed Participants", 40)
+            st.subheader("About")
+            st.write("me me me")
+
+        elif program == "RTW":
+            st.metric("Total Participants", 401)
+            st.metric("Employed Participants", 50)
+            st.subheader("About")
+            st.write("""
+            Ready to Work is San Antonio’s premier training, education, and employment program.  
+            Ready to Work will meet you where you are and take you to the next level with skills for higher-paying jobs.
+            """)
+
+    elif page == "Dashboard":
+        st.title(f"{program} Dashboard")
+
+        if program == "RTW":
+            # looking into powerbi integration
+            # 1st assigning url to variable
+            rtw_url = "https://app.powerbi.com/view?r=eyJrIjoiN2U0MzU0OTQtZDAxNi00YzY4LWE2ZGUtM2ZkNzBiMjY2MDY0IiwidCI6IjNhMjI4ZGZiLWM2NDctNDRjYi04ODM1LTdiMjA2MTdmYzkwNiIsImMiOjN9"
+            st.subheader("Interactive Power BI Dashboard")
+            components.iframe(rtw_url, height=900, width=1600)
+        else:
+            st.info("WIP")
+
+    elif page == "Map":
+        st.title(f"{program} - ZIP Interactive")
+        st.info("Map")
