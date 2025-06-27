1437be21a1065586803a02e5810a7709c7f4221b
gvg03 <127883426+gvg03@users.noreply.github.com>
2025-06-26 19:41:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -45,53 +45,7 @@ if page == "Home":
 
         st.subheader("About the RTW Program")
         st.write("BIO BLANK.")
-#home age setup
 
-# Imports
-import streamlit as st  # importing streamlit
-import pandas as pd     # panda
-import plotly
-import plotly.express as px  # for interactive zip code
-import json             # to load file, for zipcode
-import streamlit.components.v1 as components   # to bring in PowerBi
-
-# Page Setup, layout and title
-st.set_page_config(layout="wide")
-st.title("YWCA & UTSA SDS Innovation Scholars")
-
-# Sidebar: Navigate + Prog Filter
-st.sidebar.title("Program Explorer")
-page = st.sidebar.radio("View", ["Home", "Dashboard", "Map"])
-
-program = st.sidebar.selectbox("Select Program:", ["HRSA", "RESET", "RTW"])
-
-# HOME page landing
-if page == "Home":
-    st.title("YWCA & UTSA SDS Community Innovation Scholars")
-
-    # HRSA Page
-    if program == "HRSA":
-        st.metric("Total Participants", 100)
-        st.metric("Employed Participants", 20)
-
-        st.subheader("About the HRSA Program")
-        st.write("BIO BLANK.")
-
-    # RESET Page
-    elif program == "RESET":
-        st.metric("Total Participants", 200)
-        st.metric("Employed Participants", 40)
-
-        st.subheader("About the RESET Program")
-        st.write("BIO BLANK.")
-
-    # RTW Page
-    elif program == "RTW":
-        st.metric("Total Participants", 401)
-        st.metric("Employed Participants", 50)
-
-        st.subheader("About the RTW Program")
-        st.write("BIO BLANK.")
 
 
 
