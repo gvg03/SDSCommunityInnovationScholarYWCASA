a521e3e4396efa5e6bc14bf8cc8b65ef79c43571
gvg03 <127883426+gvg03@users.noreply.github.com>
2025-06-30 19:35:04 -0500

    Update RESETpg.py

--- RESETpg.py
+++ RESETpg.py
@@ -9,6 +9,14 @@ Created on Thu Jun 26 18:05:11 2025
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(layout="wide")
-st.title("Dashboard for HRSA")
+def reset_dashboard():
+    st.metric("Total Participants", 60)
+    st.metric("Employed Participants", 70)
+    st.subheader("About Ready to Work")
+    st.write(
+        ""
+    )
 
+    reset_url = ""
+    st.subheader("Interactive Power BI Dashboard")
+    components.iframe(rtw_url, height=900, width=1600)
