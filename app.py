30c017a1ccd41961b7992353cb2a7cb16a4dac9c
gvg03 <127883426+gvg03@users.noreply.github.com>
2025-07-15 10:44:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -35,9 +35,9 @@ if page == "Home":
 
     with col1:
         st.image(
-             "https://images.squarespace-cdn.com/content/v1/654d110ae6bee3661f065bbd/"
-             "a0f0fdba-f230-436e-9386-8ad375390c74/logo-orange_1_.png",
-              use_column_width=True
+            "https://images.squarespace-cdn.com/content/v1/654d110ae6bee3661f065bbd/"
+            "a0f0fdba-f230-436e-9386-8ad375390c74/logo-orange_1_.png",
+            use_container_width=True
         )
 
 
