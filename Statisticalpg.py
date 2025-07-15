827f4a5d5716c937a3be1c2ec180719c8089ac9b
gvg03 <127883426+gvg03@users.noreply.github.com>
2025-07-15 12:40:53 -0500

    Update Statisticalpg.py

--- Statisticalpg.py
+++ Statisticalpg.py
@@ -6,7 +6,7 @@ def statistical_analysis_page():
     st.subheader("What is Chi-Squared?")
 
     st.markdown("""
-    The Chi-Square Test is used to determine whether there is a **statistically significant relationship** between two categorical variables.
+    The **Chi-Square Test** is used to determine whether there is a **statistically significant relationship** between two categorical variables.
 
     In the context of this project, we use the Chi-Square test to explore whether **demographic variables** such as **age group, race, gender, or education level** are associated with **success metrics** like:
     - Program Completion
