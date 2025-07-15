fb40b3d7d41e4f84e22aa3ae9bbeb758d519058a
gvg03 <127883426+gvg03@users.noreply.github.com>
2025-07-15 12:29:21 -0500

    Update Statisticalpg.py

--- Statisticalpg.py
+++ Statisticalpg.py
@@ -3,5 +3,5 @@ import streamlit as st
 
 def statistical_analysis_page():
     st.title("Statistical Analysis: Chi-Squared")
-    st.subheader("What is Chi-Squared")
+    st.subheader("What is Chi-Squared?")
 
