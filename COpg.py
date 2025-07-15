18e6bf93cf29e83afa36fa28ff552b4a39d2e444
gvg03 <127883426+gvg03@users.noreply.github.com>
2025-07-15 14:29:34 -0500

    Update COpg.py

--- COpg.py
+++ COpg.py
@@ -2,10 +2,3 @@ import streamlit as st
 def co_page():
     st.title("Co-Enrolled Participants Overview Dashboard")
     st.metric("Total Participants", 45)
-    st.write(""
-             Co-enrollment refers to participants who were enrolled in **two or more** workforce programs — 
-             such as **Ready To Work, Community Health Worker, or RESET**. These participants may have accessed 
-             services from more than one program to enhance their employment readiness, job placement, or wage outcomes.
-             This dashboard provides insights into how participants are co-enrolled across programs
-             "")
-       
