9a2ee145e8cbc6601791d82b2afcfc2aecb24d6b
gvg03 <127883426+gvg03@users.noreply.github.com>
2025-07-16 14:27:09 -0500

    Update COpg.py

--- COpg.py
+++ COpg.py
@@ -2,3 +2,8 @@ import streamlit as st
 def co_page():
     st.title("Co-Enrolled Participants Overview Dashboard")
     st.metric("Total Participants", 45)
+
+
+    #power bi link embed 
+    co_url = ""
+    components.iframe(rtw_url, height=900, width=1600)
