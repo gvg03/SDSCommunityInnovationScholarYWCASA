1d31e199c8ba31c33103b982b3e97f867bfd9bc8
gvg03 <127883426+gvg03@users.noreply.github.com>
2025-07-21 11:44:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -110,8 +110,7 @@ elif page == "Co-Enrolled":
 ###############################################################################
 elif page == "Map":
     st.title("YWCA Workforce Programs Reach Map")
-
-    st.title(" ")
+    st.write(" ")
     # Embed your actual map here
     st.components.v1.iframe(
         "https://app.powerbi.com/view?r=eyJrIjoiYTMyMTQ1NWUtY2ZiYS00ODg1LWFiZDAtYjkwMTFhNTcyNDEzIiwidCI6IjNhMjI4ZGZiLWM2NDctNDRjYi04ODM1LTdiMjA2MTdmYzkwNiIsImMiOjN9",  # Replace with your actual map link
