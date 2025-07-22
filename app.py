f06b0d8af6c49a085286ab2e73d511cc877fb6ab
gvg03 <127883426+gvg03@users.noreply.github.com>
2025-07-22 10:47:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ if page == "Home":
 
     # intro content
     st.divider()
-    st.subheader("YWCA & UTSA SDS Community Innovation Scholars Project")
+    st.subheader("HEB Community Innovation Scholards Program with YWCA")
     st.subheader("Welcome!")
 
     st.write(
