b60a3a533e500f7f14b2a6cdb9d7d55ba2d52829
gvg03 <127883426+gvg03@users.noreply.github.com>
2025-07-28 21:41:02 -0500

    Update RESETpg.py

--- RESETpg.py
+++ RESETpg.py
@@ -16,7 +16,7 @@ def reset_dashboard():
         "RESET is designed for young women ages 16-24 who are not currently in school or working. The program supports their journey toward becoming financially, emotionally, and socially secure adults through trauma-informed case management focused on building positive connections. Through RESET, women are reengaged in education or employment by offering educational coaching and essential work readiness skills. Participants also gain access to financial literacy sessions, a women's support group, direct referrals to resources like childcare, job training, transportation, as well as life skills development in areas such as communication, self-confidence, and healthy boundaries. Enrichment opportunities, including college tours and guest speakers, are also provided to help young women successfully reconnect with their communities and move forward with confidence. "
     )
     st.markdown("""
-    <span style='font-size: 0.85rem; color: #444;'>\(*) Indicates a statistically significant relationship based on Chi-Square analysis.</span>
+    <span style='font-size: 0.85rem; color: #444;'>\(*) Indicates a statistically significant relationship based on the Chi-Square Test of Independence</span>
     """, unsafe_allow_html=True)
     
     reset_url ="https://app.powerbi.com/view?r=eyJrIjoiMDg0ZmUwNGUtZGI1MS00ZjgxLWJiNjUtOWRjNDNmMWI4N2ZmIiwidCI6IjNhMjI4ZGZiLWM2NDctNDRjYi04ODM1LTdiMjA2MTdmYzkwNiIsImMiOjN9&pageName=a10e4a667814bd0c5c78"
